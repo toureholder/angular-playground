7836f75410114a2b349d14eaf80d3985f7fa2516
Touré Holder <toureh@hotmail.com>
2022-02-19 10:31:22 -0300

    ng add @ngrx/store@latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, CoreModule],
+  imports: [AppRoutingModule, CoreModule, StoreModule.forRoot({}, {})],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
