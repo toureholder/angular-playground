27f3e7a433943bd24e49c85064a76f6d80996216
Touré Holder <toureh@hotmail.com>
2022-02-24 18:22:54 -0300

    add a progress bar component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { ProgressBarModule } from './components/progress-bar/progress-bar.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [CommonModule, ProgressBarModule],
+  exports: [ProgressBarModule],
 })
 export class SharedModule {}
